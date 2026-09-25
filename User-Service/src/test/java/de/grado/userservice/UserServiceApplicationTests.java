96663e30ddd5b15950a582ba4b938caf6847e33a
MatteoGrado <matteo.grado.23@gmail.com>
2026-09-25 09:12:38 +0200

    User-Service_Added

---
+++ User-Service/src/test/java/de/grado/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package de.grado.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
